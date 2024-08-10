a54abda0662bd8f2f2352192f3af259bb47de3c1
DwashKarma <dwaskc@gmail.com>
2024-08-10 18:03:55 +0545

    added carousel

---
+++ components/shared/CarouselCard.tsx
@@ -0,0 +1,47 @@
+"use client";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+function CarouselCard({
+  content,
+}: {
+  content: { id: number; title: string; description: string }[];
+}) {
+  return (
+    <div className="w-[100%] min-h-[10vh]">
+      <Swiper
+        slidesPerView={1}
+        // spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        // navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="lg:max-w-[80vw] max-w-[95vw] place-items-center items-center grid rounded-lg"
+      >
+        {content.map((content) => {
+          return (
+            <SwiperSlide
+              key={content.id}
+              className="text-center bg-dark grid place-content-center items-end md:max-w-[100%]   min-h-[40vh]"
+            >
+              <div className="text-2xl font-semibold">{content.title}</div>
+              <div className="text-lg font-normal">{content.description}</div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
+  );
+}
+
+export default CarouselCard;
--- components/side-bar/index.tsx
+++ components/side-bar/index.tsx
@@ -1,7 +1,29 @@
+"use client";
+import { useRouter } from "next/navigation";
+
+const links = [
+  { href: "/dashboard", label: "Dashboard" },
+  { href: "/courses", label: "Courses" },
+];
 const SideBarComponent = () => {
+  const router = useRouter();
+  const handleLink = (path: string) => {
+    router.push(path);
+  };
   return (
     <div className="w-[15%] h-[90dvh] hidden lg:flex flex-col justify-between p-4 shadow-xl border-primary border-r border-opacity-15">
-      <div>Sidebar</div>
+      <div className="flex  flex-col gap-2">
+        {links.map((item) => {
+          return (
+            <button
+              className="text-start hover:bg-light hover:shadow p-2 rounded-md hover:text-slate-100"
+              onClick={() => handleLink(item.href)}
+            >
+              <h2>{item.label}</h2>
+            </button>
+          );
+        })}
+      </div>
       <div>footer</div>
     </div>
   );
