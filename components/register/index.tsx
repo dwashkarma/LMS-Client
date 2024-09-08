d917ebb7dea96ef790598bbb167eee1a4a210ce1
DwashKarma <dwaskc@gmail.com>
2024-09-08 22:46:49 +0545

    style

--- components/register/index.tsx
+++ components/register/index.tsx
@@ -68,7 +68,7 @@ const RegisterComponent = () => {
   };
 
   return (
-    <div className="shadow border lg:w-[30%] shadow-slate-200 rounded-lg p-8  grid gap-8">
+    <div className="shadow border lg:w-[30%]  rounded-lg p-8  grid gap-8">
       <h2 className="text-center font-normal text-xl uppercase text-primary">
         Register Page
       </h2>
