c1c807e1151c2d799e9cae52bd237c53265c5750
DwashKarma <dwaskc@gmail.com>
2024-09-14 23:32:12 +0545

    FEAT: routes protected

--- app/(protected-routes)/layout.tsx
+++ app/(protected-routes)/layout.tsx
@@ -1,10 +1,19 @@
 import Layout from "@/components/layout";
+import { authOptions } from "@/libs/auth";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/");
+  }
+
   return (
     <div>
       <Layout />
